0e5ce51a0ddd8aa26e893e2cd77d22ac1e33cc99
Mateusz Pichniarczyk <mat.pich@interia.pl>
2018-05-27 23:04:13 +0200

    Add working average function (using pointers).

--- main.cpp
+++ main.cpp
@@ -2,9 +2,23 @@
 
 using namespace std;
 
+float average(int array_size, float *numbers)
+{
+    float sum = 0;
+    for(int i=0; i<array_size; i++)
+    {
+        cout<<"Type "<<i+1<<" number: "<<endl;
+        cin >>*numbers;
+        sum += *numbers;
+        numbers++;
+    }
+    return sum/array_size;
+}
+
 int main()
 {
-    cout<<"Hello World";
-
+    float *arr;
+    arr=new float[5];
+    cout <<"Average: "<<average(5,arr);
     return 0;
 }
