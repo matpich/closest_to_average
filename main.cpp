04c705d0d2460f080647d0b8b85a592e0277fc61
Mateusz Pichniarczyk <mat.pich@interia.pl>
2018-05-28 23:26:41 +0200

    First working version.

--- main.cpp
+++ main.cpp
@@ -2,13 +2,21 @@
 
 using namespace std;
 
+void user_input(int array_size, float *numbers)
+{
+    for(int i=0; i<array_size; i++)
+    {
+        cout<<"Type "<<i+1<<" number: "<<endl;
+        cin >>*numbers;
+        numbers++;
+    }
+}
+
 float average(int array_size, float *numbers)
 {
     float sum = 0;
     for(int i=0; i<array_size; i++)
     {
-        cout<<"Type "<<i+1<<" number: "<<endl;
-        cin >>*numbers;
         sum += *numbers;
         numbers++;
     }
@@ -25,11 +33,47 @@ float max_number(int array_size, float *numbers)
     }
     return highest;
 }
+float close_to_ave(int array_size, float ave, float *numbers)
+{
+    float *sec_arr, *w, lowest_dif,closest_to_ave;
+    sec_arr = new float[array_size];
+    w=sec_arr;
+
+    for(int i=0; i<array_size; i++)
+    { /// tutaj skoñczy³em
+        if((ave-*numbers)<0)
+        {
+            *w=(ave-*numbers)*(-1);
+        }
+        else
+        {
+            *w=ave-*numbers;
+        }
+        if(i==0)
+        {
+            lowest_dif=*w;
+            closest_to_ave=*numbers;
+        }
+        else if (i>0 && *w<lowest_dif)
+        {
+            lowest_dif=*w;
+            closest_to_ave=*numbers;
+        }
+        cout<<*w<<endl;
+        w++;
+        numbers++;
+    }
+    return closest_to_ave;
+}
 int main()
 {
     float *arr;
     arr=new float[5];
+
+    user_input(5,arr);
     cout <<"Average: "<<average(5,arr)<<endl;
-    cout <<"The biggest number is: "<<max_number(5,arr);
+    cout <<"The biggest number is: "<<max_number(5,arr)<<endl;
+    cout<<"Number closest to average: "<<close_to_ave(5,average(5,arr),arr);
+
     return 0;
 }
